2810fe4f4f4d5f0266bf5e882e454d878363717b
Tarik Pasalic <tpa7375@students.fhv.at>
2024-01-16 17:31:56 +0100

    objects added

--- index.js
+++ index.js
@@ -23,44 +23,47 @@ let text3 = '{"Object":[' +
 
 let text7 = '{"Object":[' +
     '{"Name":"Couch", "Description":"Das ist ein Coach!", "Start":"128", "End":"149" },' +
-    '{"Stage":1, "Start":"128", "End":"149", "X":"10", "Y":"10", "Width":"10", "Height":"10" }]}';
+    '{"Stage":1, "Start":"128", "End":"147", "X":"12", "Y":"15", "Width":"35", "Height":"30" }]}';
 
 let text8 = '{"Object":[' +
-    '{"Name":"Schrank", "Description":"Das ist ein Schrank!", "Start":"150", "End":"165" },' +
-    '{"Stage":1, "Start":"150", "End":"151", "X":"10", "Y":"10", "Width":"10", "Height":"10" },' +
-    '{"Stage":2, "Start":"152", "End":"153", "X":"20", "Y":"20", "Width":"25", "Height":"30" },' +
-    '{"Stage":3, "Start":"154", "End":"155", "X":"20", "Y":"20", "Width":"25", "Height":"30" },' +
-    '{"Stage":4, "Start":"156", "End":"157", "X":"20", "Y":"20", "Width":"25", "Height":"30" },' +
-    '{"Stage":5, "Start":"158", "End":"159", "X":"20", "Y":"20", "Width":"25", "Height":"30" }]}';
+    '{"Name":"Schrank", "Description":"Das ist ein Schrank!", "Start":"150", "End":"157" },' +
+    '{"Stage":1, "Start":"150", "End":"151", "X":"35", "Y":"10", "Width":"20", "Height":"40" },' +
+    '{"Stage":2, "Start":"152", "End":"153", "X":"31", "Y":"15", "Width":"20", "Height":"35" },' +
+    '{"Stage":3, "Start":"154", "End":"155", "X":"29", "Y":"20", "Width":"20", "Height":"25" },' +
+    '{"Stage":4, "Start":"156", "End":"157", "X":"30", "Y":"22", "Width":"20", "Height":"20" }]}';
 
 
 let text9 = '{"Object":[' +
-    '{"Name":"Lampe", "Description":"Das ist eine Lampe!", "Start":"160", "End":"165" },' +
-    '{"Stage":1, "Start":"160", "End":"162", "X":"10", "Y":"10", "Width":"10", "Height":"10" },' +
-    '{"Stage":2, "Start":"163", "End":"165", "X":"20", "Y":"20", "Width":"25", "Height":"30" }]}';
+    '{"Name":"Lampe", "Description":"Das ist eine Lampe!", "Start":"158", "End":"165" },' +
+    '{"Stage":1, "Start":"158", "End":"159", "X":"40", "Y":"10", "Width":"20", "Height":"25" },' +
+    '{"Stage":2, "Start":"160", "End":"161", "X":"40", "Y":"11", "Width":"20", "Height":"25" },' +
+    '{"Stage":2, "Start":"162", "End":"163", "X":"40", "Y":"13", "Width":"20", "Height":"25" },' +
+    '{"Stage":3, "Start":"164", "End":"165", "X":"40", "Y":"15", "Width":"20", "Height":"25" }]}';
 
 let text10 = '{"Object":[' +
     '{"Name":"Bett", "Description":"Das ist ein Bett!", "Start":"166", "End":"183" },' +
-    '{"Stage":1, "Start":"166", "End":"170", "X":"10", "Y":"10", "Width":"10", "Height":"10" },' +
-    '{"Stage":2, "Start":"171", "End":"184", "X":"20", "Y":"20", "Width":"25", "Height":"30" }]}';
+    '{"Stage":1, "Start":"166", "End":"170", "X":"20", "Y":"15", "Width":"35", "Height":"33" },' +
+    '{"Stage":2, "Start":"171", "End":"174", "X":"20", "Y":"15", "Width":"35", "Height":"33" },' +
+    '{"Stage":3, "Start":"175", "End":"184", "X":"20", "Y":"15", "Width":"38", "Height":"33" }]}';
 
 let text11 = '{"Object":[' +
     '{"Name":"Nachtkasten", "Description":"Das ist ein Nachtkasten!", "Start":"166", "End":"183" },' +
-    '{"Stage":1, "Start":"166", "End":"170", "X":"10", "Y":"10", "Width":"10", "Height":"10" },' +
-    '{"Stage":2, "Start":"171", "End":"176", "X":"20", "Y":"20", "Width":"25", "Height":"30" },' +
-    '{"Stage":3, "Start":"177", "End":"183", "X":"10", "Y":"10", "Width":"10", "Height":"10" }]}';
+    '{"Stage":1, "Start":"166", "End":"170", "X":"7", "Y":"20", "Width":"10", "Height":"25" },' +
+    '{"Stage":2, "Start":"171", "End":"175", "X":"5", "Y":"20", "Width":"10", "Height":"25" },' +
+    '{"Stage":3, "Start":"176", "End":"178", "X":"5", "Y":"20", "Width":"10", "Height":"25" },' +
+    '{"Stage":4, "Start":"179", "End":"183", "X":"7", "Y":"20", "Width":"10", "Height":"25" }]}';
 
 let text12 = '{"Object":[' +
     '{"Name":"Nachtkästchen", "Description":"Das ist ein Nachtkästchen!", "Start":"185", "End":"190" },' +
-    '{"Stage":1, "Start":"185", "End":"186", "X":"186", "Y":"10", "Width":"10", "Height":"10" },' +
-    '{"Stage":2, "Start":"187", "End":"188", "X":"20", "Y":"20", "Width":"25", "Height":"30" },' +
-    '{"Stage":3, "Start":"189", "End":"190", "X":"10", "Y":"10", "Width":"10", "Height":"10" }]}';
+    '{"Stage":1, "Start":"185", "End":"186", "X":"40", "Y":"15", "Width":"15", "Height":"25" },' +
+    '{"Stage":2, "Start":"187", "End":"188", "X":"44", "Y":"15", "Width":"15", "Height":"30" },' +
+    '{"Stage":3, "Start":"189", "End":"190", "X":"48", "Y":"15", "Width":"10", "Height":"33" }]}';
 
 
 let text13 = '{"Object":[' +
     '{"Name":"Bett", "Description":"Das ist ein Bett!", "Start":"185", "End":"195" },' +
-    '{"Stage":1, "Start":"185", "End":"186", "X":"186", "Y":"10", "Width":"10", "Height":"10" },' +
-    '{"Stage":2, "Start":"187", "End":"188", "X":"20", "Y":"20", "Width":"25", "Height":"30" },' +
+    '{"Stage":1, "Start":"185", "End":"186", "X":"3", "Y":"10", "Width":"35", "Height":"40" },' +
+    '{"Stage":2, "Start":"187", "End":"188", "X":"4", "Y":"10", "Width":"35", "Height":"40" },' +
     '{"Stage":2, "Start":"189", "End":"190", "X":"20", "Y":"20", "Width":"25", "Height":"30" },' +
     '{"Stage":2, "Start":"191", "End":"192", "X":"20", "Y":"20", "Width":"25", "Height":"30" },' +
     '{"Stage":3, "Start":"193", "End":"194", "X":"10", "Y":"10", "Width":"10", "Height":"10" }]}';
@@ -85,20 +88,22 @@ let text16 = '{"Object":[' +
     '{"Stage":2, "Start":"234", "End":"235", "X":"20", "Y":"20", "Width":"25", "Height":"30" }]}';
 
 
-const obj = JSON.parse(text);
-const obj2 = JSON.parse(text2);
-const obj3 = JSON.parse(text3);
-const obj7 = JSON.parse(text7);
-const obj8 = JSON.parse(text8);
-const obj9 = JSON.parse(text9)
-const obj10 = JSON.parse(text10);
-const obj11 = JSON.parse(text11);
-const obj12 = JSON.parse(text12);
-const obj13 = JSON.parse(text13);
-const obj14 = JSON.parse(text14);
-const obj15 = JSON.parse(text15);
+//const obj = JSON.parse(text);
+//const obj2 = JSON.parse(text2);
+//const obj3 = JSON.parse(text3);
+//const obj = JSON.parse(text7);
+//const obj2 = JSON.parse(text8);
+//const obj3 = JSON.parse(text9)
+//const obj = JSON.parse(text10);
+//const obj2 = JSON.parse(text11);
+//const obj3 = JSON.parse(text12);
+const obj = JSON.parse(text13);
+const obj2 = JSON.parse(text14);
+const obj3 = JSON.parse(text15);
 const obj16 = JSON.parse(text16);
-const objects = [obj, obj2, obj3, obj7, obj8, obj9, obj10, obj11, obj12, obj13, obj14,obj15, obj16];
+const objects = [obj, obj2, obj3, obj16];
+
+//obj7, obj8, obj9, obj10, obj11, obj12, obj13, obj14,obj15,
 
 // Not showing Buttons from the get-go
 document.getElementById("box1").style.display = "none";
